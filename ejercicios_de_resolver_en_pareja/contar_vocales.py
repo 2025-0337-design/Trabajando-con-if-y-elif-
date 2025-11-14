f487dd69fc3769fee0f1526cae58c17dac6bb3a6
Miguel Angel <mhernandezdone@gmail.com>
2025-11-13 20:05:47 -0400

    contar_vocales.py cambio 1

--- ejercicios_de_resolver_en_pareja/contar_vocales.py
+++ ejercicios_de_resolver_en_pareja/contar_vocales.py
@@ -1,13 +1,13 @@
 # ejercicio 9 Pide una palabra y muestra cuántas veces aparece cada vocal
 
 palabra = input("Escribe una palabra: ")
-palabra = palabra.lower()
+vocal = palabra.lower()
 
-a = palabra.count("a")
-e = palabra.count("e")
-i = palabra.count("i")
-o = palabra.count("o")
-u = palabra.count("u")
+a = vocal.count("a")
+e = vocal.count("e")
+i = vocal.count("i")
+o = vocal.count("o")
+u = vocal.count("u")
 
 print("Cantidad de vocales en la palabra:")
 print("a:", a)
